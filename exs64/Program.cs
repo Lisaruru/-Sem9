923284eb3947883028acafc190b1014134f1340d
m.gorshkov <m.gorshkov@skbkontur.ru>
2022-09-10 11:59:36 +0800

    add exs64

---
+++ exs64/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задайте значения M и N. Напишите программу, которая выведет 
+//все натуральные числа в промежутке от M до N от большего к меньшему.
+using System;
+
+namespace exs64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        string Number(int a, int b)
+          {
+            if(a>b)
+            {
+                return $"{a} " + Number(a-1,b);
+            }
+           if(b>a)
+            {
+               return $"{b} " + Number(b-1,a); 
+            }
+            else 
+            {
+                return $"{a}";
+            }
+           
+          }
+        Console.WriteLine(Number(5,5));
+          
+        }
+    }
+}
